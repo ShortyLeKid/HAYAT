2ead7633119188354b6bcfa0b9d543f812c54397
ShortyLeKid <xavier.dusclaux@gmail.com>
2023-08-18 17:22:27 +0200

    added b&w effect on future covers

--- src/views/AlbumList/AlbumList.jsx
+++ src/views/AlbumList/AlbumList.jsx
@@ -6,18 +6,8 @@ import './albumlist.scss';
 const AlbumList = () => {
 
     return (
-        <div className="album-list">
-            <p>
-                The HAYAT project is a platform of expression that Armless Kid established as a tribute to his mother. His goal is to release music he personally enjoys, and promote artists he genuinely appreciates.
-            </p>
-            <p>
-                Each release must carry a deep emotional vibe, allowing him to connect intimately with the music. He prefers an unfussy approach, letting the raw state of the music speak for itself.
-                HAYAT is not about being trendy nor following sophisticated vibes. It's about exploring originality and supporting artists who dare to take a little step aside from the norm.
-            </p>
-            <p>
-                It's all about keeping it simple and true to his taste.
-            </p>
-            <div>
+
+            <div className='album-list'>
                 {albumDatas.map(({
                     artist,
                     title,
@@ -40,7 +30,7 @@ const AlbumList = () => {
                     />
                 ))}
             </div>
-        </div>
+        // </div>
     );
 };
 
