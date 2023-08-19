34395fdbe18b7985debd3dd8eba61ab7916cb83c
ShortyLeKid <xavier.dusclaux@gmail.com>
2023-08-19 22:50:24 +0200

    another one..

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // Configuration pour spécifier le dossier des ressources statiques
+  build: {
+    assetsDir: 'public'
+  }
 })
