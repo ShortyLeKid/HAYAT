76df373dd97476032953057429869804dfb46fde
ShortyLeKid <xavier.dusclaux@gmail.com>
2023-08-22 19:13:44 +0200

    Added vercel's analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App/App'
-import './styles/index.scss'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App/App';
+import './styles/index.scss';
+import { BrowserRouter } from 'react-router-dom';
+import { inject } from '@vercel/analytics';
+
+inject();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     // <React.StrictMode>
