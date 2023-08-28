8b3bc12e5ed49bcf180b2617ad660efca9b4e0c1
ShortyLeKid <xavier.dusclaux@gmail.com>
2023-08-28 18:41:35 +0200

    Added label bio

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -6,20 +6,22 @@ import './home.scss';
 const Home = () => {
 
     return (
-        <div className='home_container'>
-            <div className='bio'>
+        <div className='home-container'>
+            <div className='home-container__bio'>
                 <p>
-                    The HAYAT project is a platform of expression that I established as a tribute to my mother. My goal is to release music I personally enjoy, and promote artists I genuinely appreciates.
+                    "The HAYAT project is a platform of expression that I established as a tribute to my mother. My goal is to release music I personally enjoy, and promote artists I genuinely appreciates.
                 </p>
                 <p>
-                    Each release must carry a deep emotional vibe, allowing me to connect intimately with the music. I prefers an unfussy approach, letting the raw state of the music speak for itself.
                     HAYAT is not about being trendy nor following sophisticated vibes. It's about exploring originality and supporting artists who dare to take a little step aside from the norm.
                 </p>
                 <p>
-                    It's all about keeping it simple and true to my taste.
+                    It's all about keeping it simple and true to my taste."
+                </p>
+                <p>
+                    - Armless Kid
                 </p>
             </div>
-            <div className="album-list">
+            <div className="home-container__album-list">
                 {albumDatas.map(({
                     artist,
                     title,
